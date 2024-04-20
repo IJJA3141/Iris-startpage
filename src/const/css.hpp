9a163377842c543e73ee990e72b7b535bf96ed62
IJJA3141 <alex.estella.cj@gmail.com>
2024-04-20 19:32:35 +0200

    01.01.01 compile_commands.json fix

---
+++ src/const/css.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#define CSS_ACTIVE_LABEL "css_active_label"
+#define CSS_INACTIVE_LABEL "css_inactive_label"
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
-#include "lua/config.hpp"
-
 int main(int _argc, char *_argv[])
 {
-  Iris::ConfigRetriever::get_config_retriver()->debug_config();
 
   return 0;
 }
